beec8adc93ca69109ad33a5af77a1603b986e3b2
Sumit Bajracharya <sumeetbajra@gmail.com>
2020-04-24 15:38:33 +0545

    add api fetch call for popular movies and setup env and config files

--- src/Components/PopularMoviesPage.js
+++ src/Components/PopularMoviesPage.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
 import { Row, Col, Card } from 'react-bootstrap';
 
+import config from '../config';
+
 export default class PopularMoviesPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      list: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    fetch(`${config.API_BASE_URL}/movie/popular`, { 
+      method: 'GET', 
+      headers: {
+        authorization: `Bearer ${config.ACCESS_TOKEN}`, 
+      }
+    })
+    .then(res => res.json())
+    .then(movies => {
+      this.setState({
+        list: movies.results,
+        loading: false
+      });
+    });
+  }
+
   render() {
     return (
       <div className="container-wrapper movie-detail">
---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  API_BASE_URL: 'https://api.themoviedb.org/3',
+  ACCESS_TOKEN: process.env.REACT_APP_TMDB_ACCESS_TOKEN
+}

74fb1590865d55c5ae4ea3c0af1ebd1e790ca5a6
anishachakradhar <anishachakradhar@gmail.com>
2020-04-24 14:40:13 +0545

    add movie detail page and movie list page

---
+++ src/Components/LatestMoviesPage.js
@@ -0,0 +1,188 @@
+import React, { Component } from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+
+export default class LatestMoviesPage extends Component {
+  render() {
+    return (
+      <div className="container-wrapper movie-detail">
+        <div className="movie-detail-container">
+          <h3>Latest Movies</h3>
+          <Row>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col id="movie-list-column">
+              <Card>
+                <Card.Img variant="top" src="fall.jpg" />
+                <Card.Body id="movie-list-card-body">
+                  <Card.Title id="movie-title">Movie Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    )
+  }
+}

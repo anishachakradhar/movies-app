96dbb05ea54e55c03cfb30123e6fffd0417a1581
anishachakradhar <anishachakradhar@gmail.com>
2020-07-13 18:01:36 +0545

    Updates

---
+++ precache-manifest.5f503c0cc3eacdd5c7e2a36c05695291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b86e11a49a5174da54b55dfa785936",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "b5572f13aa3b5f68768c",
+    "url": "/movies-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "6228a6c2fb39ea34273b",
+    "url": "/movies-app/static/css/main.2aa70668.chunk.css"
+  },
+  {
+    "revision": "b5572f13aa3b5f68768c",
+    "url": "/movies-app/static/js/2.8dfa647e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movies-app/static/js/2.8dfa647e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6228a6c2fb39ea34273b",
+    "url": "/movies-app/static/js/main.f1c6ac64.chunk.js"
+  },
+  {
+    "revision": "0654f2ce85ed06ec22a1",
+    "url": "/movies-app/static/js/runtime-main.0f90dc5b.js"
+  }
+]);
